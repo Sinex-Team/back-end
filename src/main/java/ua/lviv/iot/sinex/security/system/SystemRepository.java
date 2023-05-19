3af64d4b42d47d4f8c306eaa1922b5f4d4fa33ca
KuhaarTar <flempiefilms@gmail.com>
2023-05-19 22:12:14 +0300

    deleted uselles files

--- src/main/java/ua/lviv/iot/sinex/security/system/SystemRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.iot.sinex.security.system;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SystemRepository extends JpaRepository<System, Integer>{
-}
