b6ebf9aa6aeb45238c82fa49e0c6e666618a6998
KuhaarTar <flempiefilms@gmail.com>
2023-06-10 18:41:50 +0300

    delete uselles logic and add ResponseHygrometerId class

--- src/main/java/garden/aquamole/auth/AuthenticationService.java
+++ src/main/java/garden/aquamole/auth/AuthenticationService.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
